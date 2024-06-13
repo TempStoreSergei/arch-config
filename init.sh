22e386ea1d5635178567c115cc5b184dc6a41414
Sergey <123368530+TempStoreSergei@users.noreply.github.com>
2024-06-13 12:13:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
 
-# Define color codes
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
-
-# Function to display error messages in red
-error_msg() {
-    echo -e "${RED}Error: $1${NC}" >&2
-}
-
-# Function to display informational messages in yellow
-info_msg() {
-    echo -e "${YELLOW}$1${NC}"
-}
-
-# Function to display success messages in green
-success_msg() {
-    echo -e "${GREEN}$1${NC}"
-}
-
-# Function to install packages with total progress
-install_packages() {
-    local packages=("$@")
-    local total=${#packages[@]}
-    local i=0
-
-    for package in "${packages[@]}"; do
-        info_msg "Installing $package ($((i+1))/$total)..."
-        if ! sudo pacman -S --noconfirm "$package"; then
-            error_msg "Failed to install $package"
-            exit 1
-        fi
-        i=$((i+1))
-    done
-}
+# Source the functions script
+source functions.sh
 
 # Update the system
 info_msg "Updating the system..."
@@ -49,20 +15,6 @@ packages=("sway" "seatd" "python-pip" "chromium" "openssh" "nginx" "nemo" "foot"
 
 # Install packages with progress
 install_packages "${packages[@]}"
-
-# Function to enable and start a service
-enable_service() {
-    local service="$1"
-    if sudo systemctl is-enabled --quiet "$service"; then
-        info_msg "$service service is already enabled."
-    else
-        info_msg "Enabling and starting $service service..."
-        if ! sudo systemctl enable "$service" && sudo systemctl start "$service"; then
-            error_msg "Failed to enable/start $service service."
-            exit 1
-        fi
-    fi
-}
 
 # Enable and start necessary services
 enable_service "seatd"
@@ -129,4 +81,5 @@ if ! chmod +x Cloak2-Installer.sh || ! sudo ./Cloak2-Installer.sh; then
     exit 1
 fi
 
+# Reboot system prompt
 success_msg "Setup complete. Please reboot the system."
