da4430ae5b091e8e45f3fb54e7b5b4aee041c202
Sergey <123368530+TempStoreSergei@users.noreply.github.com>
2024-06-13 14:10:52 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -188,15 +188,16 @@ setup_openvpn_server() {
     info_msg "Setting up OpenVPN server..."
     
     # Fix permissions for OpenVPN files and directories
-    sudo chown -R kiosk:kiosk /etc/openvpn/server
-    sudo chmod -R 700 /etc/openvpn/server
-    sudo chown -R kiosk:kiosk /etc/openvpn/client
-    sudo chmod -R 700 /etc/openvpn/client
+    sudo chmod -R 777 /etc/openvpn/server
+    sudo chmod -R 777 /etc/openvpn/client
+
+    # Ensure proper permissions for log directory
     sudo mkdir -p /var/log/openvpn
-    sudo chown kiosk:kiosk /var/log/openvpn   # Change ownership to kiosk
-    sudo chmod 777 /var/log/openvpn
+    sudo chmod -R 777 /var/log/openvpn
+
+    # Ensure proper permissions for runtime directory
     sudo mkdir -p /run/openvpn-server
-    sudo chown kiosk:kiosk /run/openvpn-server 
+    sudo chmod -R 777 /run/openvpn-server
 
     info_msg "Initializing the PKI and building the CA..."
     cd /etc/easy-rsa || exit 1
