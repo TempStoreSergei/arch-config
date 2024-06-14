dc3f87aba2103da44027d85ebdac254ef61e479b
Sergey <you@example.com>
2024-06-14 11:10:08 +0300

    fetch: ckack packega and work with pring

--- scripts/os.sh
+++ scripts/os.sh
@@ -2,27 +2,40 @@
 #!/bin/bash
 
 # Function to install packages with total progress
+# os.sh
 install_packages() {
     local packages_file="$1"
     local packages=($(jq -r '.packages[]' "$packages_file"))
     local total=${#packages[@]}
+    local installed_packages=()
     local i=0
 
-    print_message "$YELLOW" "Installing packages:"
-
     for package in "${packages[@]}"; do
-        print_message "$YELLOW" "[$((i+1))/$total] Installing $package..."
-        if ! sudo pacman -S --noconfirm "$package"; then
-            print_message "$RED" "Failed to install $package"
-            exit 1
+        # Check if package is already installed
+        if pacman -Q "$package" &>/dev/null; then
+            print_message "$YELLOW" "[$((i+1))/$total] $package is already installed."
+        else
+            print_message "$YELLOW" "[$((i+1))/$total] Installing $package..."
+            if ! sudo pacman -S --noconfirm "$package"; then
+                print_message "$RED" "Failed to install $package"
+                exit 1
+            fi
+            installed_packages+=("$package")
         fi
         i=$((i+1))
     done
+    
 
-    print_message "$GREEN" "Packages installed successfully."
+    if [[ ${#installed_packages[@]} -gt 0 ]]; then
+        print_message "$GREEN" "Packages installed successfully: ${installed_packages[*]}"
+    else
+        print_message "$GREEN" "All required packages are already installed."
+    fi
 }
 
+
 # Function to enable and start a service
+# os.sh
 enable_service() {
     local service="$1"
     if sudo systemctl is-enabled --quiet "$service"; then
@@ -33,8 +46,8 @@ enable_service() {
             print_message "$RED" "Failed to enable/start $service service."
             exit 1
         fi
+        print_message "$GREEN" "$service service enabled and started successfully."
     fi
-    print_message "$GREEN" "$service service enabled and started successfully."
 }
 
 # Function to update the system
